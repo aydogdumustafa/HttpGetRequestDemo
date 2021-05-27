5868f1b83f084649a4b5eab2420606482a7a0d86
aydogdumustafa <69818194+aydogdumustafa@users.noreply.github.com>
2021-05-27 06:40:02 +0200

    Add project files.

---
+++ HttpGetRequestDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace HTTPGetRequestDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetRequest("https://www.microsoft.com");
+            Console.ReadKey();
+        }
+
+        async static void GetRequest(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(url))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        //string mycontent = await content.ReadAsStringAsync();
+                        HttpContentHeaders headers = content.Headers;
+
+                        Console.WriteLine(headers);
+                    }
+                }
+            }
+        }
+    }
+}
